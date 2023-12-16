ab2acbbcf93834fd7dad39cdb9589d9461d4ffef
Peter Loveland <peterloveland@PetersPsonalMBP.localdomain>
2023-12-16 11:05:28 +0000

    updated

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def connect():
         print('Waiting for connection...')
         sleep(1)
     print(wlan.ifconfig())
+    send_debug_message("Church heater is online and connected")
 
 # Function to send a message to Slack
 def send_slack_message(total_time):
@@ -22,6 +23,18 @@ def send_slack_message(total_time):
     
     message = {
         "text": "Church heater turned on for {} hours.".format(total_time)
+    }
+
+    response = urequests.post(slack_webhook_url, json=message)
+    print("Response Status Code:", response.status_code)
+    response.close()
+
+# Function to send a debug message
+def send_debug_message(message):
+    slack_webhook_url = "https://hooks.slack.com/services/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"
+    
+    message = {
+        "text": message
     }
 
     response = urequests.post(slack_webhook_url, json=message)
@@ -49,6 +62,9 @@ second_message = 3.5 * hour
 
 interval_time = 1 * hour 
 
+debug_time = 0
+debug_interval = 1 * min
+
 # send a message to slack after 1 minute, after 2 minutes, and then every 30 seconds after that.
 # every time you send a message, add the interval time to the total time
 # send this message to slack: "Pi turned on for {total_time} mins."
@@ -75,10 +91,10 @@ try :
                 total_time += 0.5
                 print("Church heater has been on for {} hours".format(total_time))
                 send_slack_message(total_time)
-        
-
+        if elapsed_time > 5 and elapsed_time % (debug_interval) == 0: # adding the 5 second delay to make sure the debug message isn't sent immediately on connection
+            debug_time += debug_interval
+            send_debug_message("Connected for {} minutes".format(debug_time // min))
         sleep(1 * second)
 
 except KeyboardInterrupt:
     machine.reset()
-
